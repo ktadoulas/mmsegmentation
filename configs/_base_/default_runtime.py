d6c420e0c50b7de7fb9b73adf84cfc31cba7f98c
Konstantinos Tantoulas <kostastad@gmail.com>
2024-08-29 23:54:53 +0200

    .

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -1,7 +1,7 @@
 default_scope = 'mmseg'
 env_cfg = dict(
     cudnn_benchmark=True,
-    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0) ,
     dist_cfg=dict(backend='nccl'),
 )
 # vis_backends = [dict(type='LocalVisBackend')]
